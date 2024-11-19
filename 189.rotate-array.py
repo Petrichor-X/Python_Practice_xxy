66b7d4e3328c1f079bd2563fea4d8a4453ab6495
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-11-18 23:54:51 -0500

    189+169

---
+++ 189.rotate-array.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=189 lang=python
+#
+# [189] Rotate Array
+#
+
+# @lc code=start
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)  # 确保 k 不超过数组长度
+        nums[:] = nums[-k:] + nums[:-k]
+
+# @lc code=end
+
